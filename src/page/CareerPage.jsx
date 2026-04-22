b964e72650f539f2393e0aa913e3f26fd68de12e
mamun-prodhan <mamunpr9@gmail.com>
2026-04-22 11:59:12 +0600

    Page content added

--- src/page/CareerPage.jsx
+++ src/page/CareerPage.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Header from '../components/Header';
+import Navbar from '../components/Navbar';
 
 const CareerPage = () => {
     return (
         <div>
-             <h2 className='text-2xl font-bold py-5'>Coming Soon...</h2>
+             <header className='py-3 w-11/12 mx-auto'>
+                <Header></Header>
+            </header>
+            <main className='w-11/12 mx-auto py-10'>
+                <h2 className='text-2xl text-center font-bold py-5'>Coming Soon...</h2>
+            </main>
         </div>
     );
 };
